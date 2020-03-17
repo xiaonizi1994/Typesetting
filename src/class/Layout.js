f1501ef0abb8267eb9c31d2267b9e3c69cf9d535
ynpan <ynpan@thoughtworks.com>
2020-03-17 14:31:49 +0800

    add template 1 logic

--- src/class/Layout.js
+++ src/class/Layout.js
@@ -1,5 +1,4 @@
 import {
-    COLUMN_HEIGHT,
     COLUMN_WIDTH,
     createFirstDiv,
     createImgDiv,
@@ -8,7 +7,6 @@ import {
 } from "../utils/renderUtil";
 import {TYPE} from "../constants";
 import {data} from "../data";
-import {Page} from "./Pages";
 
 export class Layout {
     constructor() {
@@ -113,67 +111,6 @@ export class Layout {
         }
     };
 
-    generatePages(texts, imgs) {
-        let pages = [];
-        let leftColumn = {};
-        let rightColumn = {};
-        let isAddingLeft = true;
-        let pageIndex = 0;
-        let img = imgs.shift();
-        texts.forEach((text) => {
-            if (isAddingLeft) {
-                let columnHeight = COLUMN_HEIGHT - (img ? img.height : 0);
-                if (this.isFull(columnHeight, leftColumn, text.height)) {
-                    const {preSection, nextSection} = this.splitSection(columnHeight, leftColumn, text.context);
-                    isAddingLeft = false;
-                    leftColumn = this.appendColumn(leftColumn, preSection);
-                    leftColumn = this.appendColumn(leftColumn, img);
-                    rightColumn = {
-                        totalHeight: nextSection.height,
-                        sections: [nextSection],
-                    };
-
-                    pages[pageIndex] = {
-                        leftColumn,
-                        rightColumn,
-                    };
-                    img = imgs.shift();
-                } else {
-                    leftColumn = this.appendColumn(leftColumn, text);
-                    pages[pageIndex] = {leftColumn};
-                }
-            } else {
-                let columnHeight = COLUMN_HEIGHT - (img ? img.height : 0);
-                if (this.isFull(columnHeight, rightColumn, text.height)) {
-                    const {preSection, nextSection} = this.splitSection(columnHeight, rightColumn, text.context);
-                    rightColumn = this.appendColumn(rightColumn, preSection);
-                    rightColumn = this.appendColumn(rightColumn, img);
-                    pages[pageIndex] = {
-                        ...pages[pageIndex],
-                        rightColumn,
-                    };
-                    isAddingLeft = true;
-                    pageIndex++;
-                    leftColumn = {
-                        totalHeight: nextSection.height,
-                        sections: [nextSection],
-                    };
-                    pages[pageIndex] = {
-                        leftColumn,
-                    }
-                    img = imgs.shift();
-                } else {
-                    rightColumn = this.appendColumn(rightColumn, text);
-                    pages[pageIndex] = {
-                        ...pages[pageIndex],
-                        rightColumn
-                    };
-                }
-            }
-        });
-        return pages;
-    }
-
     isFull(columnHeight, column, sectionHeight) {
         if (!column.totalHeight) {
             column.totalHeight = 0;
@@ -199,27 +136,5 @@ export class Layout {
         return columns;
     };
 
-    renderPages(pages) {
-        let pagesHtml = "";
-        pages.forEach((page, index) => {
-            let sections = page.leftColumn.sections;
-            if (sections[sections.length - 1].type === TYPE.img) {
-                const img = sections.splice(sections.length - 1, 1);
-                if (index === 0) {
-                    sections.splice(2, 0, img[0]);
-                } else {
-                    sections.splice(1, 0, img[0]);
-                }
-            }
-            pagesHtml += new Page(page.leftColumn, page.rightColumn, index).renderPage()
-        });
-        this.body.innerHTML = pagesHtml;
-    }
-
-    draw() {
-        const {texts, imgs} = this.generateDivs(data.paragraphs);
-        const pages = this.generatePages(texts, imgs);
-        this.renderPages(pages);
-    }
 
 }
---
+++ src/class/Template1.js
@@ -0,0 +1,95 @@
+import {Layout} from "./Layout";
+import {data} from "../data";
+import {COLUMN_HEIGHT} from "../utils/renderUtil";
+import {TYPE} from "../constants";
+import {Page} from "./Pages";
+
+export class Template1 extends Layout {
+    constructor() {
+        super();
+    }
+
+    generatePages(texts, imgs) {
+        let pages = [];
+        let leftColumn = {};
+        let rightColumn = {};
+        let isAddingLeft = true;
+        let pageIndex = 0;
+        let img = imgs.shift();
+        texts.forEach((text) => {
+            if (isAddingLeft) {
+                let columnHeight = COLUMN_HEIGHT - (img ? img.height : 0);
+                if (this.isFull(columnHeight, leftColumn, text.height)) {
+                    const {preSection, nextSection} = this.splitSection(columnHeight, leftColumn, text.context);
+                    isAddingLeft = false;
+                    leftColumn = this.appendColumn(leftColumn, preSection);
+                    leftColumn = this.appendColumn(leftColumn, img);
+                    rightColumn = {
+                        totalHeight: nextSection.height,
+                        sections: [nextSection],
+                    };
+
+                    pages[pageIndex] = {
+                        leftColumn,
+                        rightColumn,
+                    };
+                    img = imgs.shift();
+                } else {
+                    leftColumn = this.appendColumn(leftColumn, text);
+                    pages[pageIndex] = {leftColumn};
+                }
+            } else {
+                let columnHeight = COLUMN_HEIGHT - (img ? img.height : 0);
+                if (this.isFull(columnHeight, rightColumn, text.height)) {
+                    const {preSection, nextSection} = this.splitSection(columnHeight, rightColumn, text.context);
+                    rightColumn = this.appendColumn(rightColumn, preSection);
+                    rightColumn = this.appendColumn(rightColumn, img);
+                    pages[pageIndex] = {
+                        ...pages[pageIndex],
+                        rightColumn,
+                    };
+                    isAddingLeft = true;
+                    pageIndex++;
+                    leftColumn = {
+                        totalHeight: nextSection.height,
+                        sections: [nextSection],
+                    };
+                    pages[pageIndex] = {
+                        leftColumn,
+                    }
+                    img = imgs.shift();
+                } else {
+                    rightColumn = this.appendColumn(rightColumn, text);
+                    pages[pageIndex] = {
+                        ...pages[pageIndex],
+                        rightColumn
+                    };
+                }
+            }
+        });
+        return pages;
+    }
+
+    renderPages(pages) {
+        let pagesHtml = "";
+        pages.forEach((page, index) => {
+            let sections = page.leftColumn.sections;
+            if (sections[sections.length - 1].type === TYPE.img) {
+                const img = sections.splice(sections.length - 1, 1);
+                if (index === 0) {
+                    sections.splice(2, 0, img[0]);
+                } else {
+                    sections.splice(1, 0, img[0]);
+                }
+            }
+            pagesHtml += new Page(page.leftColumn, page.rightColumn, index).renderPage()
+        });
+        this.body.innerHTML = pagesHtml;
+    }
+
+    draw() {
+        const {texts, imgs} = this.generateDivs(data.paragraphs);
+        const pages = this.generatePages(texts, imgs);
+        this.renderPages(pages);
+    }
+}
